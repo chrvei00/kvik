e7d873893cd7933a944858cfe43e54e3bcfd2647
chrvei00 <chrvei00@gmail.com>
2021-05-08 13:57:12 +0200

    Mail-working
    Fixed mailsystem. It now works

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const expressLayouts = require("express-ejs-layouts");
 const nodemailer = require("nodemailer");
 const ejs = require("ejs");
 const helmet = require("helmet");
-const https = require("https");
 require("dotenv").config();
 //App set
 app.set("view engine", "ejs");
 //App use
 app.use(expressLayouts);
-app.use(helmet());
+//app.use(helmet());
 //Feedback-form logic
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com", //replace with your email provider
@@ -30,8 +29,9 @@ transporter.verify(function (error, success) {
     console.log("App is ready...");
   }
 });
-app.post("/submit", (req, res) => {
-  //1. Parse
+
+app.post("/send", (req, res) => {
+  //1.
   let form = new multiparty.Form();
   let data = {};
   form.parse(req, function (err, fields) {
@@ -40,21 +40,20 @@ app.post("/submit", (req, res) => {
       data[property] = fields[property].toString();
     });
 
-    //2. Design mail
+    //2. You can configure the object however you want
     const mail = {
       from: data.name,
       to: process.env.recEMAIL,
-      subject: `Tilbakemelding fra ${data.name}`,
-      html: `<h2>Tilbakemelding fra: ${data.name} - ${data.email}</h2> <h2>Svar:</h2> <h3>Kundeservice: ${data.q1Radio}</h3><h3>Rekkomendert til en venn? ${data.q2Radio}</h3><h3>Noe å legge til?: ${data.additionalText}</h3>`,
+      subject: `Tilbakemelding fra: ${data.name}`,
+      html: `<h2>Tilbakemelding fra: ${data.name} - ${data.email}</h2> <h2>Svar:</h2> <h3>Kundeservice: ${data.q1Radio}</h3><h3>Rekkomendert til en venn? ${data.q2Radio}</h3><h3>Noe å legge til?:</h3><h4> ${data.additionalText}</h4>`,
     };
-
-    //3. Send mailen
+    //3.
     transporter.sendMail(mail, (err, data) => {
       if (err) {
         console.log(err);
-        res.status(500).send("Noe gikk galt, vennligst prøv på nytt.");
+        res.status(500).send("Something went wrong.");
       } else {
-        res.status(200).send("Takk for ditt bidrag!");
+        res.status(200).send("Email successfully sent to recipient!");
       }
     });
   });
--- views/form.js
+++ views/form.js
@@ -1,23 +1,15 @@
-// Submit logic
-//get the form by its id
 const form = document.getElementById("feedback-form");
 
-//1.
 const formEvent = form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  //2.
   let mail = new FormData(form);
-
-  //3.
   sendMail(mail);
 });
 
 const sendMail = (mail) => {
-  //1.
-  fetch("http://www.rookieprogrammer.no/submit", {
-    method: "post", //2.
-    body: mail, //3.
+  fetch("http://www.rookieprogrammer.no/send", {
+    method: "post",
+    body: mail,
   }).then((response) => {
     return response.json();
   });
