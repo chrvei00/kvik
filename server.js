1eca15ffcb26a32139763c5afaaece92dbb17232
chrvei00 <chrvei00@gmail.com>
2021-05-10 01:09:14 +0200

    Added security and publicFolder

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 //Require
 const express = require("express");
 const app = express();
-const multiparty = require("multiparty");
 const expressLayouts = require("express-ejs-layouts");
-const nodemailer = require("nodemailer");
-const ejs = require("ejs");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 require("dotenv").config();
@@ -21,6 +18,16 @@ db.once("open", () => console.log("Connected to DB"));
 //App set
 app.set("view engine", "ejs");
 //App use
+app.use(
+  express.static("public", {
+    dotfiles: "ignore",
+    etag: true,
+    extensions: "ejs",
+    index: false,
+    maxAge: "7d",
+    redirect: false,
+  })
+);
 app.use(helmet());
 app.use(expressLayouts);
 app.use(express.json());
@@ -33,6 +40,8 @@ const reklamasjonRoute = require("./routes/reklamasjonRoute");
 app.use("/reklamasjon", reklamasjonRoute);
 const kontaktRoute = require("./routes/kontaktRoute");
 app.use("/kontakt", kontaktRoute);
+const dashboardRoute = require("./routes/dashboardRoute");
+app.use("/dashboard", dashboardRoute);
 
 //Launch server on Port
 app.listen(process.env.PORT, () => {
