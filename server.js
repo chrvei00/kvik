d9587ce70a7d7d78d325de4726e60eb296a22698
chrvei00 <chrvei00@gmail.com>
2021-05-23 16:13:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -32,8 +32,7 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to DB"));
 //App use (standard middleware)
 app.use(
-  "/static",
-  express.static("public", {
+  express.static(path.join(__dirname, "../public/"), {
     dotfiles: "ignore",
     etag: true,
     extensions: "ejs",
